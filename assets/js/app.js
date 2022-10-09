0b633d14cd2328f41e9a11e04244c7d0c0493d52
Nikita Endea <90311455+Nikita-Endeka2004@users.noreply.github.com>
2022-10-09 17:48:39 +0300

    nocomlite

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,35 +5,70 @@ const url = "https://fakestoreapi.com/products";
 let data = await fetch(url);
     data = await data.json();
 
-    let cardsForJs = document.getElementById('card_inner');
+    const model = {
 
-    cardsForJs.innerHTML = data.map( (item) => `
+      sort: null,
+
+      doSort(direction){
+
+        this.sort = direction;
+        this.output();
+
+      },
+
+      output(){
+
+        if(this.sort){
+          if(this.sort == 'up'){
+              ratesToShow.sort((a, b) => a.rate - b.rate);
+          }else if(this.sort == 'down'){
+              ratesToShow.sort((a, b) => b.rate - a.rate);
+          }
+      }
+
+        let cardsForJs = document.getElementById('card_inner');
+
+        cardsForJs.innerHTML = data.map( (item) => `
+        
+        <div class="card m-2 p-2 text-justify">
     
-    <div class="card m-2 p-2 text-justify" id="faf">
+                <div class="midle">
+    
+                    <img src="${item.image}" class="m-2 rounded p-2" style="max-width: 100%;" alt="...">
+    
+                </div>
+    
+                <div class="card-body">
+    
+                  <h5 class="card-title">${item.category}</h5>
+                  <p class="card-text">${item.description.substring(0, 50) + (item.description.lenght > 50 ? '...' : '')}</p>
+    
+                </div>
+    
+                <strong class="font-weight-bold text-end pl-3">${item.price} $</strong>
+    
+        </div>
+    
+        
+        `).join('');
 
-            <div class="midle">
+      }
 
-                <img src="${item.image}" class="m-2 rounded p-2" style="max-width: 100%;" alt="...">
+    }
 
-            </div>
 
-            <div class="card-body">
+    let down = document.getElementById('button-down');
+    let up = document.getElementById('button-up');
 
-              <h5 class="card-title">${item.category}</h5>
-              <p class="card-text">${item.description}</p>
+    up.addEventListener('click', function(){
+      model.doSort('up');
+  });
 
-            </div>
-
-            <strong class="font-weight-bold text-end pl-3">${item.price} $</strong>
-
-    </div>
+  down.addEventListener('click', function(){
+    model.doSort('down');
+});
 
     
-    `).join('');
-
-
-
-    let MaxCostInCard = document.documentElement('squaredThree');
 
         
   
